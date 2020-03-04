0eaa8f408e8f9c55425f008874befbcfe3250062
Alok Saxena <aloksaxenagit@gmail.com>
2020-03-04 17:16:37 +0530

    New ARM Project

---
+++ ARMExamples/Deploy-storageVnet.ps1
@@ -0,0 +1,27 @@
+﻿### Define Deployment Variables
+{
+$location = 'Australia Southeast'
+$resourceGroupName = 'contoso-storage-iaas-template'
+$resourceDeploymentName = 'contoso-storage-iaas-template-deployment'
+#$templatePath = $env:SystemDrive + '\' + 'pluralsight'
+$templatePath = 'D:\Github\Alok-01\ArmTemplateExample\ARMExamples'
+$templateFile = 'storageVnetDeploy.json'
+$template = $templatePath + '\' + $templateFile
+}
+
+### Create Resource Group
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $location `
+    -Verbose -Force
+}
+
+### Deploy Resources
+{
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -Verbose -Force
+}
