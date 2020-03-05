a730bb0c2caa27a69363b9192b1740bd5dded435
Alok Saxena <aloksaxenagit@gmail.com>
2020-03-05 16:42:33 +0530

    VM+Storage+LoadBalance

--- ARMExamples/Deploy-storageVnet.ps1
+++ ARMExamples/Deploy-storageVnet.ps1
@@ -1,8 +1,8 @@
 ﻿### Define Deployment Variables
 {
 $location = 'Australia Southeast'
-$resourceGroupName = 'contoso-storage-iaas-template'
-$resourceDeploymentName = 'contoso-storage-iaas-template-deployment'
+$resourceGroupName = 'triangle-storage-iaas-template'
+$resourceDeploymentName = 'triangle-storage-iaas-template-deployment'
 #$templatePath = $env:SystemDrive + '\' + 'pluralsight'
 $templatePath = 'D:\Github\Alok-01\ArmTemplateExample\ARMExamples'
 $templateFile = 'storageVnetDeploy.json'
@@ -23,5 +23,41 @@ New-AzureRmResourceGroupDeployment `
     -Name $resourceDeploymentName `
     -ResourceGroupName $resourceGroupName `
     -TemplateFile $template `
+    -Verbose -Force
+}
+
+### Define Deployment Variables
+{
+$location = 'Australia Southeast'
+$resourceGroupName = 'triangle-iaas'
+$resourceDeploymentName = 'triangle-iaas-deployment'
+#$templatePath = $env:SystemDrive + '\' + 'pluralsight'
+#$templateFile = 'triangleIaas.json'
+$templatePath = 'D:\Github\Alok-01\ArmTemplateExample\ARMExamples'
+$templateFile = 'storageVnetDeploy.json'
+$template = $templatePath + '\' + $templateFile
+$password = "abc@123"
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+}
+
+### Create Resource Group
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $location `
+    -Verbose -Force
+}
+
+### Deploy Resources
+{
+
+$additionalParameters = New-Object -TypeName Hashtable
+$additionalParameters['vmAdminPassword'] = $securePassword
+
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    @additionalParameters `
     -Verbose -Force
 }
